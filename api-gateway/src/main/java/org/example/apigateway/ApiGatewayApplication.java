e634818fc3917ac96e3dd7d525145de7d7e14b50
chedi <chedi.bouali@esprit.tn>
2025-04-08 15:00:00 +0100

    chedi

---
+++ api-gateway/src/main/java/org/example/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,26 @@
+package org.example.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("reclamations-service", r -> r.path("/api/reclamations/**")
+                        .uri("lb://NOMPRENOMCLASSEEXAMEN"))
+                .build();
+    }
+
+}
